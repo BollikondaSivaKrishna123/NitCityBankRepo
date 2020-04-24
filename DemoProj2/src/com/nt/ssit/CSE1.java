c2c9121ba8b9f5ac7520e5d70d995674e4554f98
Delp1 <DELL@WELCOME>
2020-04-24 19:09:22 +0530

    TL ADDED

---
+++ DemoProj2/src/com/nt/ssit/CSE1.java
@@ -0,0 +1,25 @@
+package com.nt.ssit;
+class demo1
+{
+	public void m1()
+	{
+System.out.println("hii");
+}
+	public void m2()
+	{
+System.out.println("hii vamsi");
+}
+	public void m3()
+	{
+System.out.println("hii naga raju");
+}
+	public void m4()
+	{
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	}
+
