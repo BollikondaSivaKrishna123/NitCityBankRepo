04f4f624e9add4a0bf7b0f8b8ab48ecbce8638d2
Delp1 <DELL@WELCOME>
2020-04-24 06:44:34 +0530

    TL updated

--- DemoProj2/src/com/nt/ssit/Demo1.java
+++ DemoProj2/src/com/nt/ssit/Demo1.java
@@ -5,6 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("BOLLIKONDA SIVA KRISHNA");
+System.out.println("BOLLIKONDA VAMSI KRISHNA");
 	}
 
 }
