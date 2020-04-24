2081d4a6ea25720c92eb4c25d66385c50d805abe
Delp1 <DELL@WELCOME>
2020-04-24 19:00:20 +0530

    BRACH1-DEV1 UPDATED

--- DemoProj2/src/com/nt/ssit/Demo3.java
+++ DemoProj2/src/com/nt/ssit/Demo3.java
@@ -5,6 +5,8 @@ public class Demo3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Brach1-TL-MODULE");
+		System.out.print("Brach2-DEV1-MODULE");
+System.out.print("Brach2-MODULE");
 
 	}
 
