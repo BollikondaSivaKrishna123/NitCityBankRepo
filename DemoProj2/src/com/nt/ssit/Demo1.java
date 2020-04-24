4de533afa07fc3c4b088b081076497a6a7b93a5d
Delp1 <DELL@WELCOME>
2020-04-24 18:00:06 +0530

    TL added m2() method

--- DemoProj2/src/com/nt/ssit/Demo1.java
+++ DemoProj2/src/com/nt/ssit/Demo1.java
@@ -1,5 +1,11 @@
 package com.nt.ssit;
-
+class Demo11
+{
+	public void m2()
+	{
+		System.out.println("Thank you ssit students...");
+	}
+}
 public class Demo1 {
 
 	public static void main(String[] args) {
@@ -9,8 +15,8 @@ System.out.println("BOLLIKONDA VAMSI KRISHNA");
 System.out.println("WELCOME TO SAI SPURTHI ENGG COLLEGAE");
 System.out.println("Thank you ra");
 System.out.println("RATUDUPALEM");
-
-
+Demo11 s=new Demo11();
+s.m2();
 
 	}
 
