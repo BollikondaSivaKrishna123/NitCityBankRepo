22106bc8d6685717f8e8ad7398fdd42a509b38e9
Delp1 <DELL@WELCOME>
2020-04-24 18:24:29 +0530

    Dev1 Updated

--- DemoProj2/src/com/nt/ssit/Demo1.java
+++ DemoProj2/src/com/nt/ssit/Demo1.java
@@ -4,6 +4,8 @@ class Demo11
 	public void m2()
 	{
 		System.out.println("Thank you ssit students...vamsi@123");
+		System.out.println("Thank you ssit students...nagaraju@123");
+		
 	}
 }
 public class Demo1 {
@@ -15,6 +17,7 @@ System.out.println("BOLLIKONDA VAMSI KRISHNA");
 System.out.println("WELCOME TO SAI SPURTHI ENGG COLLEGAE");
 System.out.println("Thank you ra");
 System.out.println("RATUDUPALEM");
+
 Demo11 s=new Demo11();
 s.m2();
 
