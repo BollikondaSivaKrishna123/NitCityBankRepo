a21c6a6c5d7c3d272ca8fd465b9dad6d51405a9c
Delp1 <DELL@WELCOME>
2020-04-24 20:05:52 +0530

    ss

--- DemoProj2/src/com/nt/ssit/CSE1.java
+++ DemoProj2/src/com/nt/ssit/CSE1.java
@@ -1,32 +1,5 @@
 package com.nt.ssit;
-<<<<<<< HEAD
-class demo1
-{
-	public void m1()
-	{
-System.out.println("hii");
-}
-	public void m2()
-	{
-System.out.println("hii vamsi");
-}
-	public void m3()
-	{
-System.out.println("hii naga raju");
-}
-	public void m4()
-	{
-}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	}
-
-=======
-
-public class CSE1 {
+	public class CSE1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -35,4 +8,3 @@ public class CSE1 {
 	}
 
 }
->>>>>>> branch 'master' of https://github.com/BollikondaSivaKrishna123/NitCityBankRepo.git
